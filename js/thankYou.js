5255890ad7ab0ef278efc31c2ac7bcc9a1a50aa7
bekzodfronten <bs3328545@gmail.com>
2025-12-15 16:05:12 +0500

    push

---
+++ js/thankYou.js
@@ -0,0 +1,39 @@
+async function sendFormData() {
+  const formDataRaw = localStorage.getItem("formData");
+  if (!formDataRaw) {
+    return;
+  }
+
+  const formDataObj = JSON.parse(formDataRaw);
+
+
+  // Prepare FormData for API
+  const formData = new FormData();
+  formData.append("sheetName", "Lead");
+  formData.append("Ism", formDataObj.Ism);
+  formData.append("Telefon raqam", formDataObj.TelefonRaqam);
+  formData.append("Royhatdan o'tgan vaqti", formDataObj.SanaSoat);
+
+  try {
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbzby9HLVE9hY_SUlXfuiCoIlHO8TbuYTVA-fFWNSE0my08vgigDP-M2d6VaI-_Eq_a4/exec",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+    
+    
+    if (response.ok) {
+      localStorage.removeItem("formData");
+    } else {
+      throw new Error("API response was not ok");
+    }
+  } catch (error) {
+    console.error("Error submitting form:", error);
+    document.getElementById("errorMessage").style.display = "block";
+  }
+}
+
+// Send data when page loads
+window.onload = sendFormData;
